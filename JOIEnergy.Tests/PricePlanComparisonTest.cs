5c692122433e118fa73ea07cbff0a58f3382295c
dmoore <dmoore@thoughtworks.com>
2018-01-11 13:36:36 +0000

    Populate initial data for API

---
+++ JOIEnergy.Tests/PricePlanComparisonTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JOIEnergy.Tests
+{
+    public class PricePlanComparisonTest
+    {
+
+    }
+}
--- JOIEnergy/Startup.cs
+++ JOIEnergy/Startup.cs
@@ -27,8 +27,8 @@ namespace JOIEnergy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var readings = 
-                new Dictionary<string, List<ElectricityReading>>();
+            var readings =
+                GenerateMeterElectricityReadings();
 
             var pricePlans = new List<PricePlan> {
                 new PricePlan{
