88625d13bbee75e9e3e1c3a76adb787bb775e455
antrikshdua <dua.antriksh@gmail.com>
2024-02-27 00:42:23 +0530

    Refactored logic for readability

--- JOIEnergy/Services/AccountService.cs
+++ JOIEnergy/Services/AccountService.cs
@@ -6,15 +6,10 @@ using JOIEnergy.Interfaces;
 namespace JOIEnergy.Services
 {
     public class AccountService(Dictionary<string, Supplier> smartMeterToPricePlanAccounts) : Dictionary<string, Supplier>, IAccountService
-    { 
-        private Dictionary<string, Supplier> _smartMeterToPricePlanAccounts = smartMeterToPricePlanAccounts;
-
-        public Supplier GetPricePlanIdForSmartMeterId(string smartMeterId) {
-            if (!_smartMeterToPricePlanAccounts.ContainsKey(smartMeterId))
-            {
-                return Supplier.NullSupplier;
-            }
-            return _smartMeterToPricePlanAccounts[smartMeterId];
+    {
+        public Supplier GetPricePlanIdForSmartMeterId(string smartMeterId)
+        {
+            return smartMeterToPricePlanAccounts.GetValueOrDefault(smartMeterId, Supplier.NullSupplier);
         }
     }
 }
--- JOIEnergy/Services/MeterReadingService.cs
+++ JOIEnergy/Services/MeterReadingService.cs
@@ -10,19 +10,18 @@ namespace JOIEnergy.Services
     {
         public Dictionary<string, List<ElectricityReading>> MeterAssociatedReadings { get; set; } = meterAssociatedReadings;
 
-        public List<ElectricityReading> GetReadings(string smartMeterId) {
-            if (MeterAssociatedReadings.ContainsKey(smartMeterId)) {
-                return MeterAssociatedReadings[smartMeterId];
-            }
-            return new List<ElectricityReading>();
+        public List<ElectricityReading> GetReadings(string smartMeterId)
+        {
+            return MeterAssociatedReadings.TryGetValue(smartMeterId, out var reading) ? reading : [];
         }
 
         public void StoreReadings(string smartMeterId, List<ElectricityReading> electricityReadings) {
-            if (!MeterAssociatedReadings.ContainsKey(smartMeterId)) {
-                MeterAssociatedReadings.Add(smartMeterId, new List<ElectricityReading>());
+            if (!MeterAssociatedReadings.TryGetValue(smartMeterId, out var value)) {
+                value = [];
+                MeterAssociatedReadings.Add(smartMeterId, value);
             }
 
-            electricityReadings.ForEach(electricityReading => MeterAssociatedReadings[smartMeterId].Add(electricityReading));
+            electricityReadings.ForEach(electricityReading => value.Add(electricityReading));
         }
     }
 }
